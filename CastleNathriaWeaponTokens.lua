adb217b420d81ce50461735f41c866b69c403523
OldGromm <oldgromm@protonmail.com>
2025-07-30 10:10:32 +0200

    Add files via upload

---
+++ CastleNathriaWeaponTokens.lua
@@ -0,0 +1,363 @@
+-- Database (Start)
+
+
+---- ItemIDs
+CN_KY_G_0 = { 174298, 174310, 175251, 175254, 175279, 176098, 177850, 178973, 180312, 184230, 184236 }
+CN_NE_G_0 = { 184244, 184245, 184246, 184247, 184248, 184249, 184250, 184251, 184252, 184253, 184254 }
+CN_NF_G_0 = { 179497, 179527, 179544, 179557, 179570, 179577, 179610, 180000, 180023, 180073, 180260 }
+CN_VE_G_0 = { 182414, 182415, 182416, 182417, 182418, 182420, 182422, 182423, 182424, 182425, 182426 }
+
+
+---- ModifiedAppearanceID
+------ Kyrian (LFR/Normal/Heroic)
+CN_KY_N_1 = { 108552, 108556, 115593, 115595 }
+CN_KY_N_2 = { 108565, 108569 }
+CN_KY_N_3 = { 108908, 111013, 115585, 115591 }
+CN_KY_N_4 = { 108910, 111018 }
+CN_KY_N_5 = { 108930, 110997, 115502, 115512, 115587, 115588, 115589, 115590, 115708, 115710 }
+CN_KY_N_6 = { 109545, 111008 }
+CN_KY_N_7 = { 110998, 111003 }
+CN_KY_N_8 = { 111619, 111621 }
+CN_KY_N_9 = { 112964, 112967 }
+CN_KY_N_10 = { 111020, 115499 }
+CN_KY_N_11 = { 115526, 115535 }
+
+------ Necrolord (LFR/Normal/Heroic)
+CN_NE_N_1 = { 115537, 115549 }
+CN_NE_N_2 = { 115538, 115550 }
+CN_NE_N_3 = { 115539, 115551 }
+CN_NE_N_4 = { 115540, 115552 }
+CN_NE_N_5 = { 115541, 115553 }
+CN_NE_N_6 = { 115542, 115554 }
+CN_NE_N_7 = { 115543, 115555 }
+CN_NE_N_8 = { 115544, 115556 }
+CN_NE_N_9 = { 115545, 115557 }
+CN_NE_N_10 = { 115546, 115548, 115558, 115560 }
+CN_NE_N_11 = { 115547, 115559 }
+
+------ Night Fae (LFR/Normal/Heroic)
+CN_NF_N_1 = { 112277, 112282 }
+CN_NF_N_2 = { 112312, 112315 }
+CN_NF_N_3 = { 112325, 112328 }
+CN_NF_N_4 = { 112341, 112345, 114088, 115533 }
+CN_NF_N_5 = { 112350, 112354 }
+CN_NF_N_6 = { 112361, 112363 }
+CN_NF_N_7 = { 112394, 112395 }
+CN_NF_N_8 = { 112825, 112827 }
+CN_NF_N_9 = { 112841, 112842 }
+CN_NF_N_10 = { 112860, 112862 }
+CN_NF_N_11 = { 112957, 112959 }
+
+------ Venthyr (LFR/Normal/Heroic)
+CN_VE_N_1 = { 114104, 114130 }
+CN_VE_N_2 = { 114105, 114131 }
+CN_VE_N_3 = { 114106, 114132 }
+CN_VE_N_4 = { 114107, 114133 }
+CN_VE_N_5 = { 114108, 114109, 114134, 114135 }
+CN_VE_N_6 = { 114110, 114111, 114136, 114137 }
+CN_VE_N_7 = { 114112, 114138 }
+CN_VE_N_8 = { 114113, 114139 }
+CN_VE_N_9 = { 114114, 114140 }
+CN_VE_N_10 = { 114115, 114141 }
+CN_VE_N_11 = { 114116, 114142 }
+
+------ Kyrian (Mythic)
+CN_KY_M_1 = { 115498, 115509, 115594, 115596, 115704, 115705 }
+CN_KY_M_2 = { 115507, 115517 }
+CN_KY_M_3 = { 115500, 115510, 115586, 115592, 115706, 115707, 145966 }
+CN_KY_M_4 = { 115506, 115516 }
+CN_KY_M_5 = { 108930, 110997, 115502, 115512, 115587, 115588, 115589, 115590, 115708, 115710 }
+CN_KY_M_6 = { 115503, 115513, 115711 }
+CN_KY_M_7 = { 115501, 115511, 115709 }
+CN_KY_M_8 = { 115508, 115518, 115732 }
+CN_KY_M_9 = { 115504, 115514, 115712 }
+CN_KY_M_10 = { 115505, 115515, 115731 }
+CN_KY_M_11 = { 115527, 115529, 115536 }
+
+------ Necrolord (Mythic)
+CN_NE_M_1 = { 115563, 115575, 115755 }
+CN_NE_M_2 = { 115569, 115583, 115753 }
+CN_NE_M_3 = { 115567, 115578, 115752 }
+CN_NE_M_4 = { 115566, 115581, 115760 }
+CN_NE_M_5 = { 115572, 115584, 115762 }
+CN_NE_M_6 = { 115570, 115579, 115759 }
+CN_NE_M_7 = { 115565, 115577, 115764 }
+CN_NE_M_8 = { 115561, 115573, 115763 }
+CN_NE_M_9 = { 115568, 115582, 115761 }
+CN_NE_M_10 = { 115562, 115571, 115574, 115580, 115754 }
+CN_NE_M_11 = { 115564, 115576, 115756 }
+
+------ Night Fae (Mythic)
+CN_NF_M_1 = { 115005, 115487, 115719 }
+CN_NF_M_2 = { 115011, 115494, 115727 }
+CN_NF_M_3 = { 115003, 115493, 115726 }
+CN_NF_M_4 = { 115008, 115009, 115490, 115534, 115721, 115722, 184040 }
+CN_NF_M_5 = { 115010, 115496, 115729 }
+CN_NF_M_6 = { 115006, 115488, 115720 }
+CN_NF_M_7 = { 115002, 115497, 115730  }
+CN_NF_M_8 = { 115012, 115495, 115728 }
+CN_NF_M_9 = { 115013, 115491, 115723, 184034 }
+CN_NF_M_10 = { 115004, 115492, 115725 }
+CN_NF_M_11 = { 115007, 115489, 115724 }
+
+------ Venthyr (Mythic)
+CN_VE_M_1 = { 114864, 115049, 115735 }
+CN_VE_M_2 = { 115055, 115056, 115745 }
+CN_VE_M_3 = { 114865, 115050, 115736 }
+CN_VE_M_4 = { 114860, 115044, 115737 }
+CN_VE_M_5 = { 114862, 114866, 115047, 115051, 115743, 115744 }
+CN_VE_M_6 = { 114863, 115048, 115057, 115058, 115739, 115741 }
+CN_VE_M_7 = { 114861, 115045, 115738 }
+CN_VE_M_8 = { 114867, 115052, 115742 }
+CN_VE_M_9 = { 114868, 115053, 115746 }
+CN_VE_M_10 = { 114869, 115054, 115734 }
+CN_VE_M_11 = { 114859, 115046, 115733 }
+
+
+---- Index Tables
+CN_KY_N_INDEX = { CN_KY_N_1, CN_KY_N_2, CN_KY_N_3, CN_KY_N_4, CN_KY_N_5, CN_KY_N_6, CN_KY_N_7, CN_KY_N_8, CN_KY_N_9, CN_KY_N_10, CN_KY_N_11 }
+CN_NE_N_INDEX = { CN_NE_N_1, CN_NE_N_2, CN_NE_N_3, CN_NE_N_4, CN_NE_N_5, CN_NE_N_6, CN_NE_N_7, CN_NE_N_8, CN_NE_N_9, CN_NE_N_10, CN_NE_N_11 }
+CN_NF_N_INDEX = { CN_NF_N_1, CN_NF_N_2, CN_NF_N_3, CN_NF_N_4, CN_NF_N_5, CN_NF_N_6, CN_NF_N_7, CN_NF_N_8, CN_NF_N_9, CN_NF_N_10, CN_NF_N_11 }
+CN_VE_N_INDEX = { CN_VE_N_1, CN_VE_N_2, CN_VE_N_3, CN_VE_N_4, CN_VE_N_5, CN_VE_N_6, CN_VE_N_7, CN_VE_N_8, CN_VE_N_9, CN_VE_N_10, CN_VE_N_11 }
+CN_KY_M_INDEX = { CN_KY_M_1, CN_KY_M_2, CN_KY_M_3, CN_KY_M_4, CN_KY_M_5, CN_KY_M_6, CN_KY_M_7, CN_KY_M_8, CN_KY_M_9, CN_KY_M_10, CN_KY_M_11 }
+CN_NE_M_INDEX = { CN_NE_M_1, CN_NE_M_2, CN_NE_M_3, CN_NE_M_4, CN_NE_M_5, CN_NE_M_6, CN_NE_M_7, CN_NE_M_8, CN_NE_M_9, CN_NE_M_10, CN_NE_M_11 }
+CN_NF_M_INDEX = { CN_NF_M_1, CN_NF_M_2, CN_NF_M_3, CN_NF_M_4, CN_NF_M_5, CN_NF_M_6, CN_NF_M_7, CN_NF_M_8, CN_NF_M_9, CN_NF_M_10, CN_NF_M_11 }
+CN_VE_M_INDEX = { CN_VE_M_1, CN_VE_M_2, CN_VE_M_3, CN_VE_M_4, CN_VE_M_5, CN_VE_M_6, CN_VE_M_7, CN_VE_M_8, CN_VE_M_9, CN_VE_M_10, CN_VE_M_11 }
+
+
+---- Buttons
+CN_BTN = {
+    -- Kyrian (LFR/Normal/Heroic)
+    { ["ButtonID"] = 1, ["spell"] = 321076, ["diff"] = BINDING_HEADER_OTHER, ["anchor"] = "TOPLEFT", ["positionx"] = 15, ["positiony"] = -35, ["covenant"] = "SanctumUpgrades-Kyrian-32x32", ["difficon"] = "GM-icon-difficulty-normal-hover"},
+    -- Kyrian (Mythic)
+    { ["ButtonID"] = 2, ["spell"] = 321076, ["diff"] = PLAYER_DIFFICULTY6, ["anchor"] = "TOPRIGHT", ["positionx"] = -15, ["positiony"] = -35, ["covenant"] = "SanctumUpgrades-Kyrian-32x32", ["difficon"] = "GM-icon-difficulty-mythic-hover"},
+    -- Necrolord (LFR/Normal/Heroic)
+    { ["ButtonID"] = 3, ["spell"] = 321078, ["diff"] = BINDING_HEADER_OTHER, ["anchor"] = "TOPLEFT", ["positionx"] = 15, ["positiony"] = -60, ["covenant"] = "SanctumUpgrades-Necrolord-32x32", ["difficon"] = "GM-icon-difficulty-normal-hover"},
+    -- Necrolord (Mythic)
+    { ["ButtonID"] = 4, ["spell"] = 321078, ["diff"] = PLAYER_DIFFICULTY6, ["anchor"] = "TOPRIGHT", ["positionx"] = -15, ["positiony"] = -60, ["covenant"] = "SanctumUpgrades-Necrolord-32x32", ["difficon"] = "GM-icon-difficulty-mythic-hover"},
+    -- Night Fae (LFR/Normal/Heroic)
+    { ["ButtonID"] = 5, ["spell"] = 321077, ["diff"] = BINDING_HEADER_OTHER, ["anchor"] = "TOPLEFT", ["positionx"] = 15, ["positiony"] = -85, ["covenant"] = "SanctumUpgrades-NightFae-32x32", ["difficon"] = "GM-icon-difficulty-normal-hover"},
+    -- Night Fae (Mythic)
+    { ["ButtonID"] = 6, ["spell"] = 321077, ["diff"] = PLAYER_DIFFICULTY6, ["anchor"] = "TOPRIGHT", ["positionx"] = -15, ["positiony"] = -85, ["covenant"] = "SanctumUpgrades-NightFae-32x32", ["difficon"] = "GM-icon-difficulty-mythic-hover"},
+    -- Venthyr (LFR/Normal/Heroic)
+    { ["ButtonID"] = 7, ["spell"] = 321079, ["diff"] = BINDING_HEADER_OTHER, ["anchor"] = "TOPLEFT", ["positionx"] = 15, ["positiony"] = -110, ["covenant"] = "SanctumUpgrades-Venthyr-32x32", ["difficon"] = "GM-icon-difficulty-normal-hover"},
+    -- Venthyr (Mythic)
+    { ["ButtonID"] = 8, ["spell"] = 321079, ["diff"] = PLAYER_DIFFICULTY6, ["anchor"] = "TOPRIGHT", ["positionx"] = -15, ["positiony"] = -110, ["covenant"] = "SanctumUpgrades-Venthyr-32x32", ["difficon"] = "GM-icon-difficulty-mythic-hover"},
+}
+
+-- Database (End)
+
+
+
+-- Window (Frame)
+local CastleFrame = CreateFrame("Frame", "MyAddonCastleFrame", ParentUI, "BasicFrameTemplateWithInset")
+      CastleFrame:SetSize(400, 445)
+      CastleFrame:SetPoint("CENTER", UIParent, "CENTER", 0, 0)
+      CastleFrame.TitleBg:SetHeight(30)
+      CastleFrame.title = CastleFrame:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
+      CastleFrame.title:SetPoint("CENTER", CastleFrame.TitleBg, "TOP", 0, -11)
+      CastleFrame.title:SetText("Castle Nathria Weapon Tokens")
+
+	  -- Covenant Icon
+      CastleFrame.Covenant = CastleFrame:CreateTexture()
+	  CastleFrame.Covenant:SetPoint("TOP", MyAddonCastleFrame, "TOP", -30, -135)
+	  CastleFrame.Covenant:SetSize(32, 32)
+	  CastleFrame.Covenant:SetAtlas("TransparentSquareMask")
+
+	  -- Difficulty Icon
+      CastleFrame.Difficulty = CastleFrame:CreateTexture()
+	  CastleFrame.Difficulty:SetPoint("TOP", MyAddonCastleFrame, "TOP", 30, -124)
+	  CastleFrame.Difficulty:SetSize(55, 55)
+	  CastleFrame.Difficulty:SetAtlas("TransparentSquareMask")
+
+      CastleFrame.Weapons = CastleFrame:CreateFontString(nil, "OVERLAY", "GameFontNormalLeft")
+      CastleFrame.Weapons:SetPoint("TOPLEFT", CastleFrame, "TOPLEFT", 20, -175)
+      CastleFrame.Weapons:SetText(" ")
+
+      CastleFrame:Hide()
+      CastleFrame:EnableMouse(true)
+      CastleFrame:SetMovable(true)
+      CastleFrame:RegisterForDrag("LeftButton")
+      CastleFrame:SetScript("OnDragStart", function(self)
+          self:StartMoving()
+end)
+      CastleFrame:SetScript("OnDragStop", function(self)
+          self:StopMovingOrSizing()
+end)
+
+if CastleFrame:IsShown() then
+    CastleFrame:Hide()
+else
+    CastleFrame:Show()
+end
+
+
+-- Window (Content) (Start)
+local function UpdateList (ButtonID)
+    -- Reset Global Variables
+    CN_FLAG_FINAL = false
+    CN_TABLE_NAMES = {}
+    CN_TABLE_APPEARANCE = {}
+    CN_ITEMSET = CN_KY_G_0
+    CN_APPEARANCESET1 = CN_KY_N_INDEX
+	CN_APPEARANCESET2 = CN_KY_N_1
+	CN_FLAG_TEMP = false
+	CN_TABLE_MAX = 0
+    CN_WINDOW_LIST = ""
+	CN_WEAPONS_TEMP = ""
+	local CN_BUTTON_ID = ButtonID
+
+
+	-- Reset icons and weapon list.
+	CastleFrame.Covenant:SetAtlas("TransparentSquareMask")
+	CastleFrame.Difficulty:SetAtlas("TransparentSquareMask")
+	CastleFrame.Weapons:SetText("")
+
+
+	for i, CNV in pairs(CN_BTN) do
+	    if i == CN_BUTTON_ID then
+			CastleFrame.Covenant:SetAtlas(CNV["covenant"])
+	        CastleFrame.Difficulty:SetAtlas(CNV["difficon"])
+		else
+		end
+    end	
+
+
+    -- Assign variables depending on Button ID
+	-- (I know this is not the most elegant approach, assigning everything manually. However, nothing else I tried to dynamically assign the variables worked.)
+    if CN_BUTTON_ID == 1 then
+	    CN_ITEMSET = CN_KY_G_0
+		CN_APPEARANCESET1 = CN_KY_N_INDEX
+	elseif CN_BUTTON_ID == 2 then
+	    CN_ITEMSET = CN_KY_G_0
+		CN_APPEARANCESET1 = CN_KY_M_INDEX
+	elseif CN_BUTTON_ID == 3 then
+	    CN_ITEMSET = CN_NE_G_0
+		CN_APPEARANCESET1 = CN_NE_N_INDEX
+	elseif CN_BUTTON_ID == 4 then
+	    CN_ITEMSET = CN_NE_G_0
+		CN_APPEARANCESET1 = CN_NE_M_INDEX
+	elseif CN_BUTTON_ID == 5 then
+	    CN_ITEMSET = CN_NF_G_0
+		CN_APPEARANCESET1 = CN_NF_N_INDEX
+	elseif CN_BUTTON_ID == 6 then
+	    CN_ITEMSET = CN_NF_G_0
+		CN_APPEARANCESET1 = CN_NF_M_INDEX
+	elseif CN_BUTTON_ID == 7 then
+	    CN_ITEMSET = CN_VE_G_0
+		CN_APPEARANCESET1 = CN_VE_N_INDEX
+	elseif CN_BUTTON_ID == 8 then
+	    CN_ITEMSET = CN_VE_G_0
+		CN_APPEARANCESET1 = CN_VE_M_INDEX
+	else
+	end
+
+
+	-- Save ItemIDs to a table.
+    for i=1, 11, 1 do
+	    table.insert(CN_TABLE_NAMES, C_Item.GetItemNameByID(CN_ITEMSET[i]))
+    end
+
+
+	-- Save ModifiedAppearanceID to a table.
+    for i=1, 11, 1 do
+	    CN_APPEARANCESET2 = CN_APPEARANCESET1[i]
+		CN_TABLE_MAX = table.getn(CN_APPEARANCESET2)
+		CN_FLAG_FINAL = false
+		for i=1, CN_TABLE_MAX, 1 do
+			CN_FLAG_TEMP = C_TransmogCollection.PlayerHasTransmogItemModifiedAppearance(CN_APPEARANCESET2[i])
+			if CN_FLAG_TEMP == true then
+	            CN_FLAG_FINAL = true
+	        else
+	        end
+		end
+		table.insert(CN_TABLE_APPEARANCE, CN_FLAG_FINAL)
+    end
+
+
+    for i=1, 11, 1 do
+		-- Check for faulty data
+	    if CN_TABLE_NAMES[i] == nil then
+		    CastleFrame.Weapons:SetText("Error loading data. Please try again.")
+	    else
+	    end
+		if CN_TABLE_APPEARANCE[i] == nil then
+		    CastleFrame.Weapons:SetText("Error loading data. Please try again.")
+	    else
+	    end
+
+
+        -- Check for weapon appearance collected.
+        local CNTEXTCOLOR = CreateColor(0.996, 0.882, 0.361, 1.000) -- Yellow (Not Collected)
+		if CN_TABLE_APPEARANCE[i] == true then
+            CNTEXTCOLOR = CreateColor(0.251, 0.753, 0.251, 1.000) -- Green (Collected)
+	    else
+	    end
+
+
+	    -- Apply color to weapon name (either yellor for not collected, or green for collected).
+		CN_WEAPONS_TEMP = CNTEXTCOLOR:WrapTextInColorCode(CN_TABLE_NAMES[i])
+	    CN_WINDOW_LIST = (CN_WINDOW_LIST..CN_WEAPONS_TEMP.."\n\n")
+	end
+
+
+	-- Create weapon name list in frame.
+	CastleFrame.Weapons:SetText(CN_WINDOW_LIST)
+end
+-- Window (Content) (End)
+
+
+
+-- Buttons (Moriaz!)
+for i, CNV in pairs(CN_BTN) do
+	local CastleButton = CreateFrame("Button", nil, MyAddonCastleFrame, "UIPanelButtonTemplate")
+    CastleButton:SetPoint(CNV["anchor"], CNV["positionx"], CNV["positiony"])
+    CastleButton:SetSize(180, 20)
+    CastleButton:SetText(C_Spell.GetSpellName(CNV["spell"]) .. " - " .. CNV["diff"])
+    CastleButton:SetScript("OnClick", function()
+		UpdateList(i)
+    end)
+    CastleButton:RegisterForClicks("AnyUp")
+end
+
+
+
+-- Slash command
+SLASH_NATHRIA1 = "/castle"
+SLASH_NATHRIA2 = "/nathria"
+SLASH_NATHRIA3 = "/cn"
+
+SlashCmdList.NATHRIA = function()
+    if CastleFrame:IsShown() then
+        CastleFrame:Hide()
+    else
+        CastleFrame:Show()
+    end
+end
+
+table.insert(UISpecialFrames, "MyAddonCastleFrame")
+
+
+
+-- Compartment
+AddonCompartmentFrame:RegisterAddon({
+    text = "Castle Nathria Weapon Tokens",
+    icon = 3614361,
+    notCheckable = true,
+    func = function(button, menuInputData, menu)
+    local buttonName = menuInputData.buttonName;
+        if CastleFrame:IsShown() then
+            CastleFrame:Hide()
+        else
+            CastleFrame:Show()
+        end
+    end,
+    funcOnEnter = function(button)
+        MenuUtil.ShowTooltip(button, function(tooltip)
+            tooltip:SetText("Displays which weapon appearances from the Castle Nathria raid have been unlocked.")
+        end)
+    end,
+    funcOnLeave = function(button)
+        MenuUtil.HideTooltip(button)
+    end,
+})
